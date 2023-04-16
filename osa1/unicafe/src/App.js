7ea212459799f1e255b9fae3f7bb48ea22350dfc
Janne Kähkönen <jm.kahkonen@gmail.com>
2023-04-16 18:46:35 +0300

    1.6: unicafe step1

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const DisplayHeader = ({header}) => <h1>{header}</h1>
+
+const Button = ({ handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <div>
+        <DisplayHeader header="give feedback" />
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      </div>
+      <div>
+        <DisplayHeader header="statistics" />
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
