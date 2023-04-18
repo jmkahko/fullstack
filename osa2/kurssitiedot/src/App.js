9df34f6da9344fcd119377e032bda4411047be04
Janne Kähkönen <jm.kahkonen@gmail.com>
2023-04-18 19:22:23 +0300

    2.5: erillinen moduuli

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,47 +1,4 @@
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h1>{course.name}</h1>
-    </div>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(course => 
-        <Part key={course.id} course={course} />
-      )}
-      <TotalCount course={course} />
-    </div>
-  )
-}
-
-const Part = ({ course }) => (
-  <>
-    <p>{course.name} {course.exercises}</p>
-  </>
-)
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-    </div>
-  )
-}
-
-const TotalCount = ({ course }) => {
-  let luku = 0
-  course.parts.map(value => {
-    luku += value.exercises
-  })
-
-  return (
-    <b>total of {luku} exercises</b>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
