d76e044c05f01f33016e5475b4b74861d70df054
Janne Kähkönen <jm.kahkonen@gmail.com>
2023-04-23 18:24:43 +0300

    2.13: puhelinluettelo step8, tehty 2.12 yhteydessä

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import personServices from './services/personsService'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
